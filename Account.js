d53cff602c38bb14164e1dc4984764671846caa4
Shih hao Lin <lincecum.lin@concentrus.com>
2019-02-17 01:32:59 +0800

    Refactor

--- Account.js
+++ Account.js
@@ -1,9 +1,52 @@
 export class Account {
-    getAmount(first, last) {
+    allBudget = [];
 
+    constructor(iBudgetRepo) {
+        if(iBudgetRepo){
+            this.allBudget = iBudgetRepo.getAll();
+        }
+    }
+
+    /**
+     *
+     * @param first
+     * @param last
+     * @return {number}
+     */
+    first = null;
+    last = null;
+    getAmount(first, last) {
+        this.first = first;
+        this.last = last;
         if (first.isAfter(last)) {
             return 0.00;
         }
-        return 0.00;
+
+        const extFirst = first.clone().startOf('month');
+        const extLast = last.clone().endOf('month');
+        let total = null;
+        while (extLast > extFirst) {
+            total += this.getBudgetAmount(extFirst);
+            extFirst.add(1, 'month');
+        }
+        total -= this.excludeFirstMonth();
+        total -= this.excludeLastMonth();
+        return total || 0.00;
+    }
+
+    excludeLastMonth(last = this.last) {
+        return this.getBudgetAmount(last) * (last.daysInMonth() - last.date()) / last.daysInMonth();
+    }
+
+    excludeFirstMonth(first = this.first) {
+        return this.getBudgetAmount(first) * (first.date() - 1) / first.daysInMonth();
+    }
+
+    getBudgetAmount(moment) {
+        return this.allBudget[Account.getAccountYearMonth(moment)];
+    }
+
+    static getAccountYearMonth(moment) {
+        return moment.format('YYYYMM');
     }
 }
--- Account.test.js
+++ Account.test.js
@@ -1,27 +1,25 @@
 import {Account} from "./Account";
 import moment from 'moment';
+import {Budget} from "./Budget";
+import {IBudgetRepo} from "./IBudgetRepo";
 
 let account = new Account();
 let start = null;
 let end = null;
 beforeEach(() => {
-    account = new Account();
     // clean house
     start = null;
     end = null;
 });
-test('Test No Budget', () => {
-    start = createMoment(2019, 1, 1);
-    end = createMoment(2019, 1, 1);
-    amountShouldBe(0.00);
-});
 
 function amountShouldBe(expected) {
+
     expect(account.getAmount(start, end)).toBe(expected);
 }
 
 function createMoment(year, month, day) {
-    return moment({year, month, day});
+
+    return moment({year, month: month - 1, day});
 }
 
 test('Test Wrong Date', () => {
@@ -29,5 +27,55 @@ test('Test Wrong Date', () => {
     end = createMoment(2019, 1, 1);
     amountShouldBe(0.00);
 });
+
+test('Test No Budget', () => {
+    const iBudgetRepo = new IBudgetRepo([]);
+    account = new Account(iBudgetRepo);
+    start = createMoment(2019, 1, 1);
+    end = createMoment(2019, 1, 1);
+    amountShouldBe(0.00);
+});
+
+test('Test Whole Month', () => {
+    const iBudgetRepo = new IBudgetRepo([
+        new Budget('201901', 31)
+    ]);
+    account = new Account(iBudgetRepo);
+    start = createMoment(2019, 1, 1);
+    end = createMoment(2019, 1, 31);
+    amountShouldBe(31.00);
+});
+
+test('Test One Day', () => {
+    const iBudgetRepo = new IBudgetRepo([
+        new Budget('201901', 31)
+    ]);
+    account = new Account(iBudgetRepo);
+    start = createMoment(2019, 1, 1);
+    end = createMoment(2019, 1, 1);
+    amountShouldBe(1.00);
+});
+test('Test Cross Month', () => {
+    const iBudgetRepo = new IBudgetRepo([
+        new Budget('201901', 31),
+        new Budget('201902', 280)
+    ]);
+    account = new Account(iBudgetRepo);
+    start = createMoment(2019, 1, 31);
+    end = createMoment(2019, 2, 1);
+    amountShouldBe(11.00);
+});
+test('Test Cross Month', () => {
+    const iBudgetRepo = new IBudgetRepo([
+        new Budget('201901', 31),
+        new Budget('201902', 280),
+        new Budget('201903', 31000)
+    ]);
+    account = new Account(iBudgetRepo);
+    start = createMoment(2019, 1, 31);
+    end = createMoment(2019, 3, 1);
+    amountShouldBe(1281.00);
+});
+
 
 
