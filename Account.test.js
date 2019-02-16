c623eb357df1dd2dee239c1a8a1ef911d19b8b4b
Shih hao Lin <lincecum.lin@concentrus.com>
2019-02-17 00:15:40 +0800

    No budget case

---
+++ Account.test.js
@@ -0,0 +1,10 @@
+import {Account} from "./Account";
+import moment from 'moment';
+
+test('Test No Budget', () => {
+
+    let account = new Account();
+    const start = moment({year: 2019, month: 2, day: 1});
+    const end = moment({year: 2019, month: 2, day: 1});
+    expect(account.getAmount(start, end)).toBe(0.00);
+});
