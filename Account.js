52a63ac08f18a11de6de3351bb882a387ec18179
Shih hao Lin <lincecum.lin@concentrus.com>
2019-02-17 01:16:27 +0800

    One Day Case

--- Account.js
+++ Account.js
@@ -1,9 +1,27 @@
 export class Account {
+    _iBudgetRepo;
+
+    constructor(iBudgetRepo) {
+        this._iBudgetRepo = iBudgetRepo;
+    }
+
+    /**
+     *
+     * @param first {moment}
+     * @param last
+     * @return {number}
+     */
     getAmount(first, last) {
 
         if (first.isAfter(last)) {
             return 0.00;
         }
-        return 0.00;
+        const allBudget = this._iBudgetRepo.getAll();
+        const diffDay = Math.abs(first.diff(last, 'days')) + 1;
+        return allBudget[this.getAccountYearMonth(first)] / first.daysInMonth() * diffDay || 0.00;
+    }
+
+    getAccountYearMonth(moment) {
+        return moment.format('YYYYMM');
     }
 }
