1ca94cc2f0042635bf5fccc8659d967ac9df1680
Shih hao Lin <lincecum.lin@concentrus.com>
2019-02-17 00:21:07 +0800

    Wrong Date Case

--- Account.js
+++ Account.js
@@ -1,5 +1,9 @@
 export class Account {
     getAmount(first, last) {
+
+        if (first.isAfter(last)) {
+            return 0.00;
+        }
         return 0.00;
     }
 }
--- Account.test.js
+++ Account.test.js
@@ -8,3 +8,11 @@ test('Test No Budget', () => {
     const end = moment({year: 2019, month: 2, day: 1});
     expect(account.getAmount(start, end)).toBe(0.00);
 });
+test('Test Wrong Date', () => {
+
+    let account = new Account();
+    const start = moment({year: 2019, month: 2, day: 1});
+    const end = moment({year: 2019, month: 1, day: 1});
+    expect(account.getAmount(start, end)).toBe(0.00);
+});
+
